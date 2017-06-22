23c3d885bf95f276a79aa266a448266a6d25b7db
speedcoder96 <rene.t.sommerfeld@gmail.com>
2017-06-22 10:59:23 +0200

    ShutDownHookThread moved to inner class

--- Blatt 9 A1/src/SizeUpdateTimerTaskCreator.java
+++ Blatt 9 A1/src/SizeUpdateTimerTaskCreator.java
@@ -10,7 +10,6 @@ import java.util.TimerTask;
 public class SizeUpdateTimerTaskCreator {
 
     public static void main(String[] args) {
-        System.out.println(args[0]);
         //ensures that only a program start containing one argument
         //can execute a FileSizeUpdateTimerTask
         if(args.length == 1) {
@@ -33,6 +32,39 @@ public class SizeUpdateTimerTaskCreator {
             //error case
             System.err.println("Program needs a path for a file object to listen to!");
             System.out.println("Use java SizeUpdateTimerTaskCreator File/Directory");
+        }
+    }
+
+    /**
+     * Only ensures that the timer is canceled when the program
+     * gets terminated via CTRL-C. This thread gets added as
+     * ShutDownHook to the Runtime.
+    **/
+    private static class ShutDownHookThread extends Thread {
+
+        /**
+         * the file where the FileSizeUpdateTimerTask listens to
+         */
+        private File file;
+        /**
+         * the timer that schedules the TimerTask
+         */
+        private Timer timer;
+
+        /**
+         * Constructor
+         * @param timer the timer
+         * @param file the file
+         */
+        public ShutDownHookThread(Timer timer, File file) {
+            this.timer = timer;
+            this.file = file;
+        }
+
+        @Override
+        public void run() {
+            timer.cancel();
+            System.out.println("No longer listen to " + file.getName());
         }
     }
 
